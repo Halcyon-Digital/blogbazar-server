1b12ae49a58bb43e56e333533f3aad8049603e24
sharif291 <talha.halcyon@gmail.com>
2022-09-04 12:37:17 +0600

    complite authentication

--- controller/auth.js
+++ controller/auth.js
@@ -1,10 +1,9 @@
-const express = require("express");
-const router = express.Router();
+const bcrypt = require("bcrypt");
 const User = require("../models/userModel");
 const { registerService } = require("../service/auth");
 const generateToken = require("../service/token");
 
-router.post("/auth/reg", async (req, res) => {
+const register = async (req, res) => {
   const { firstName, lastName, userName, email, password, status } = req.body;
   if (!firstName || !lastName || !userName || !email || !password) {
     return res.status(400).json({ message: "Invalid Data" });
@@ -23,7 +22,6 @@ router.post("/auth/reg", async (req, res) => {
       lastName,
       userName,
       email,
-      password,
       status,
     });
 
@@ -35,6 +33,37 @@ router.post("/auth/reg", async (req, res) => {
   } catch (error) {
     console.log(error);
   }
-});
+};
 
-module.exports = router;
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "Invalid Data" });
+  }
+
+  // Check for user email
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(404).json({ message: "User Not Found!" });
+  }
+
+  const userData = {
+    firstName: user.firstName,
+    lastName: user.lastName,
+    userName: user.userName,
+    email: user.email,
+    status: user.status,
+  };
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({ ...userData, token: generateToken(user._id) });
+  } else {
+    res.status(400).json({ message: "Invalid Password" });
+  }
+};
+
+module.exports = {
+  register,
+  login,
+};
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 require("dotenv").config();
 const app = express();
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 8000;
 
@@ -12,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.use("/api/v1/users", require("./routes/userRoutes"));
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Server Listening on http://localhost:${PORT}`);
